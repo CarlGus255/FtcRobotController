3045b828779aa72adef408ad0047779c6fa76386
CarlGus255 <carlgus255@gmail.com>
2026-03-18 01:59:11 -0500

    Create HelloWorld.java
    Created First Tutorial Program from Brogan M. Pratt's video Learn java for Robotics: Your First Program (FTC)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+// Single Line Comment
+/*
+Block Comment
+
+A class is a collection of code that the computer can execute(?)
+A method is a specific function type thing
+Public means other thins can see it
+Extends means that it includes other things that it imports
+Imports import bits of code, such as a program to tell a servo to go to a certain value
+Void means something
+
+We need at least an init(), which is what it runs when we hit init on gamepad, and a loop(), which it loops through when we press run.
+Telemetry is the values you can see on the driver hub (Think system.out.println())
+@Teleop means its a teleop program
+@Override means it overrides something (I don't remember)
+
+Adding @Disabled - makes it not show up in program selections
+ */
+@TeleOp
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello: ", "Carl");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
+/*
+1. Change form "Hello World" to "Hello: Carl"
+
+2. Run in autonomouns (@Autonomous instead of @Teleop)
+ */
