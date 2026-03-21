288bf5b8550b7082a88e04c1783f0c2b2d5f372d
CarlGus255 <carlgus255@gmail.com>
2026-03-21 02:06:40 -0500

    Heading Seek Diffy Swerve
    Added a V2 class, where instead of continous human heading control, it seeks to a given heading. For testing, this is incremented with the X button. There's an acceleration value which determines how fast it seeks to the value.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarlDiffySwervePod.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name="CarlDiffySwervePod", group="CarlDiffySwerve")
+public class CarlDiffySwervePod extends OpMode {
+
+    double translation = 0;
+    double rotation = 0;
+    double heading = 0;
+    double wheelVelocity = 0;
+    boolean isHoming = false;
+
+
+
+    MotorsCarlDiffySwerve motors = new MotorsCarlDiffySwerve();
+
+    @Override
+    public void init() {
+        motors.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        //link translation and rotation variables to gamepad
+        translation = gamepad1.left_stick_y;
+        rotation = gamepad1.right_stick_y;
+
+        //print rotation and translation values
+        telemetry.addData("Translation is:", translation);
+        telemetry.addData("Rotation is:", rotation);
+
+        //get the current wheel heading in degrees
+        heading = (motors.getTopMotorPos() + motors.getBottomMotorPos()) * 2 * 360;
+        //print heading value
+        telemetry.addData("Heading is:", heading);
+
+        //testing switch
+        telemetry.addData("Switch is off:", motors.getHomingSwitchState());
+
+        //get the forward velocity of the wheel
+        wheelVelocity = motors.getTopMotorVelocity()- motors.getBottomMotorVelocity();
+        telemetry.addData("Wheel Velocity is:", wheelVelocity);
+
+
+        //We need to have a function that when the y button is pressed, it rotates the pod until it hits the switch.
+        if (gamepad1.y) {
+            isHoming = true;
+        }
+
+        if (isHoming && motors.getHomingSwitchState()) {
+            //starts the homing sequence if we are in homing mode and switch is not depressed (Not at home yet)
+            rotation = 0.1;
+            telemetry.addData("We are:", "Homing!");
+        }
+        if (isHoming && !motors.getHomingSwitchState()){
+            //stops the homing sequence if we are in homing mode but the switch gets depressed (Now at home)
+            //stop rotation
+            rotation = 0;
+            isHoming = false;
+
+            //reset motors
+            motors.resetBottomMotor();
+            motors.resetTopMotor();
+
+            //telemetry for sanity's sake
+            telemetry.addData("Motors are:", "Reset!");
+            telemetry.addData("We are:", "Not Homing!");
+        }
+
+        //tells if you are in the homing sequence or not on gamepad
+        telemetry.addData("isHoming is", isHoming);
+
+        //set motor power based on translation and rotation values
+        motors.setTopMotor(translation + rotation);
+        motors.setBottomMotor(-translation + rotation);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarlDiffySwerveRobotPodV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarlDiffySwerveRobotPodV2.java
@@ -2,14 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp(name="CarlDiffySwerve", group="CarlDiffySwervePlan")
-public class CarlDiffySwerve extends OpMode {
+@TeleOp(name="CarlDiffySwervePodV2", group="CarlDiffySwervePlan")
+public class CarlDiffySwerveRobotPodV2 extends OpMode {
 
     double translation = 0;
     double rotation = 0;
     double heading = 0;
     double wheelVelocity = 0;
     boolean isHoming = false;
+    double seekHeading = 0;
+    double proportionalHeading = 0;
+    boolean lastX = false;
+    double headingDifference = 90;
+    double rotationAcceleration = 1;
+
 
 
     MotorsCarlDiffySwerve motors = new MotorsCarlDiffySwerve();
@@ -23,25 +29,34 @@ public class CarlDiffySwerve extends OpMode {
     public void loop() {
 
         translation = gamepad1.left_stick_y;
-        rotation = gamepad1.right_stick_y;
-        
+
+        if (gamepad1.x && !lastX) {
+            seekHeading = seekHeading + headingDifference;
+        }
+        lastX = gamepad1.x;
+
+        proportionalHeading = (heading-seekHeading)/(headingDifference*2);
+        rotation = -proportionalHeading * rotationAcceleration;
+
+        telemetry.addData("Seek heading is:", seekHeading);
+        telemetry.addData("proportional gain is:", proportionalHeading);
+
+        //print rotation and translation values
         telemetry.addData("Translation is:", translation);
         telemetry.addData("Rotation is:", rotation);
 
-
+        //get the current wheel heading in degrees
         heading = (motors.getTopMotorPos() + motors.getBottomMotorPos()) * 2 * 360;
-
+        //print heading value
         telemetry.addData("Heading is:", heading);
 
+        //testing switch
         telemetry.addData("Switch is off:", motors.getHomingSwitchState());
+
+        //get the forward velocity of the wheel
         wheelVelocity = motors.getTopMotorVelocity()- motors.getBottomMotorVelocity();
         telemetry.addData("Wheel Velocity is:", wheelVelocity);
 
-        if (!motors.getHomingSwitchState()) {
-            motors.resetBottomMotor();
-            motors.resetTopMotor();
-            telemetry.addData("Motors are:", "Reset!");
-        }
 
         //We need to have a function that when the y button is pressed, it rotates the pod until it hits the switch.
         if (gamepad1.y) {
@@ -49,24 +64,33 @@ public class CarlDiffySwerve extends OpMode {
         }
 
         if (isHoming && motors.getHomingSwitchState()) {
-            rotation = 0.2;
+            //starts the homing sequence if we are in homing mode and switch is not depressed (Not at home yet)
+            rotation = 0.1;
             telemetry.addData("We are:", "Homing!");
         }
         if (isHoming && !motors.getHomingSwitchState()){
+            //stops the homing sequence if we are in homing mode but the switch gets depressed (Now at home)
+            //stop rotation
             rotation = 0;
             isHoming = false;
+
+            //reset motors
+            motors.resetBottomMotor();
+            motors.resetTopMotor();
+
+            //telemetry for sanity's sake
+            telemetry.addData("Motors are:", "Reset!");
             telemetry.addData("We are:", "Not Homing!");
         }
 
+
+
+        //tells if you are in the homing sequence or not on gamepad
         telemetry.addData("isHoming is", isHoming);
 
+        //set motor power based on translation and rotation values
         motors.setTopMotor(translation + rotation);
         motors.setBottomMotor(-translation + rotation);
     }
 
 }
-
-
-
-
-
