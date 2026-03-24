3d413d9e998f7b6b2432872aeb013a5510a694e6
CarlGus255 <carlgus255@gmail.com>
2026-03-23 23:02:46 -0500

    Update CarlTeleOpClass.java
    Made odometry more testable by adding telemetry to record field position and a button to attempt field repositioning with the camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarlCoaxSwerve/CarlTeleOpClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarlCoaxSwerve/CarlTeleOpClass.java
@@ -11,11 +11,15 @@ public class CarlTeleOpClass extends OpMode {
     WebCamCarlCoaxSwerve cam = new WebCamCarlCoaxSwerve();
     MotorsCarlCoaxSwervePractice motors = new MotorsCarlCoaxSwervePractice();
 
+    //Velocity values for teleop implementation
     double xVelocity;
     double yVelocity;
     double yawVelocity;
+    //lastY for state detection for gamepad 1, so driver a can just press Y to activtae run to position 1, which is defined in swerve implement.
     boolean lastY = false;
     boolean runPos1 = false;
+    //last2y for gamepad 2 y. This is to test reseting the field variables with the camera
+    boolean last2y = false;
 
     public void init() {
         //Initialize the camera, odometry, and mechanisms
@@ -52,6 +56,16 @@ public class CarlTeleOpClass extends OpMode {
             //Camera update?
             runPos1 = false;
         }
+        //Prints Field variables for testing (Cm)
+        telemetry.addData("Field X is:", swerveImp.fieldX);
+        telemetry.addData("Field Y is:", swerveImp.fieldY);
+        telemetry.addData("Field Yaw is:", swerveImp.fieldYaw);
 
+        //Test camera reset
+        if (gamepad2.y && !last2y) {
+            odo.resetFieldWebcamRedBin();
+            telemetry.addData("Field is:", "Reset!");
+        }
+        last2y = gamepad2.y;
     }
 }
